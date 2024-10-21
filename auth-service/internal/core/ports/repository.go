5d47fb25af970b7b32f3fb9b9813a55a16ce5145
oguzhang <oguzhanguldibi@gmail.com>
2024-10-21 16:58:21 +0300

    add define repositories

---
+++ auth-service/internal/core/ports/repository.go
@@ -0,0 +1,16 @@
+package ports
+
+import (
+	"github.com/google/uuid"
+	"github.com/ncfex/dcart/auth-service/internal/domain"
+)
+
+type UserRepository interface {
+	FindByUsername(username string) (*domain.User, error)
+	Create(user *domain.User) error
+}
+
+type TokenRepository interface {
+	StoreToken(userID uuid.UUID, token string) error
+	ValidateToken(token string) (int64, error)
+}
