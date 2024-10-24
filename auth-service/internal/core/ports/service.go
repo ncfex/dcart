8f2280f63b5c6d1e612a8ae4edb358963ea00669
oguzhang <oguzhanguldibi@gmail.com>
2024-10-24 20:37:29 +0300

    renamed and added new core service interfaces

--- auth-service/internal/core/ports/service.go
+++ auth-service/internal/core/ports/service.go
@@ -2,11 +2,23 @@ package ports
 
 import (
 	"context"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/ncfex/dcart/auth-service/internal/domain"
 )
 
-type Authenticator interface {
+type UserAuthenticator interface {
 	Register(ctx context.Context, username string, password string) (*domain.User, error)
 	Login(ctx context.Context, username string, password string) (string, error)
+}
+
+type PasswordEncrypter interface {
+	Hash(password string) (string, error)
+	Compare(hashedPassword, password string) error
+}
+
+type TokenManager interface {
+	Make(userID uuid.UUID, expiresIn time.Duration) (string, error)
+	Validate(token string) (uuid.UUID, error)
 }
