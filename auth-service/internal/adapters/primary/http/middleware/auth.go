435cf5dd76741163ab09291fd87a6a5c46f6fa25
oguzhang <oguzhanguldibi@gmail.com>
2024-11-03 20:19:01 +0300

    update auth middleware

--- auth-service/internal/adapters/primary/http/middleware/auth.go
+++ auth-service/internal/adapters/primary/http/middleware/auth.go
@@ -1,12 +1,16 @@
 package middleware
 
 import (
+	"context"
+	"errors"
 	"net/http"
+	"time"
 
 	"github.com/ncfex/dcart/auth-service/internal/adapters/primary/http/request"
 	"github.com/ncfex/dcart/auth-service/internal/core/ports"
 )
 
+// TODO consider response.Responder
 func AuthenticateWithJWT(
 	tokenManager ports.TokenManager,
 	tokenRepo ports.TokenRepository,
@@ -14,6 +18,9 @@ func AuthenticateWithJWT(
 ) Middleware {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx, cancel := context.WithTimeout(r.Context(), time.Second*5)
+			defer cancel()
+
 			accessToken, err := request.GetBearerToken(r.Header)
 			if err != nil {
 				http.Error(w, "not authorized", http.StatusUnauthorized)
@@ -26,13 +33,18 @@ func AuthenticateWithJWT(
 				return
 			}
 
-			user, err := userRepo.GetUserByID(r.Context(), userID)
+			user, err := userRepo.GetUserByID(ctx, userID)
 			if err != nil {
-				http.Error(w, "not authorized", http.StatusUnauthorized)
+				switch {
+				case errors.Is(err, context.DeadlineExceeded):
+					http.Error(w, "request timeout", http.StatusGatewayTimeout)
+				default:
+					http.Error(w, "not authorized", http.StatusUnauthorized)
+				}
 				return
 			}
 
-			ctx := request.SetValueToContext(r.Context(), request.UserIDContextKey, userID)
+			ctx = request.SetValueToContext(ctx, request.UserIDContextKey, userID)
 			ctx = request.SetValueToContext(ctx, request.UserContextKey, user)
 
 			next.ServeHTTP(w, r.WithContext(ctx))
@@ -47,19 +59,27 @@ func AuthenticateWithRefreshToken(
 ) Middleware {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx, cancel := context.WithTimeout(r.Context(), time.Second*5)
+			defer cancel()
+
 			refreshToken, err := request.GetBearerToken(r.Header)
 			if err != nil {
 				http.Error(w, "not authorized", http.StatusUnauthorized)
 				return
 			}
 
-			user, err := tokenRepo.GetUserFromToken(r.Context(), refreshToken)
+			user, err := tokenRepo.GetUserFromToken(ctx, refreshToken)
 			if err != nil {
-				http.Error(w, "not authorized", http.StatusUnauthorized)
+				switch {
+				case errors.Is(err, context.DeadlineExceeded):
+					http.Error(w, "request timeout", http.StatusGatewayTimeout)
+				default:
+					http.Error(w, "not authorized", http.StatusUnauthorized)
+				}
 				return
 			}
 
-			ctx := request.SetValueToContext(r.Context(), request.UserIDContextKey, user.ID)
+			ctx = request.SetValueToContext(ctx, request.UserIDContextKey, user.ID)
 			ctx = request.SetValueToContext(ctx, request.UserContextKey, user)
 
 			next.ServeHTTP(w, r.WithContext(ctx))
