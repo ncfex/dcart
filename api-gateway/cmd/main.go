5cd8f20275a764d4eba2d1b8fdd8674849a4b1b6
oguzhang <oguzhanguldibi@gmail.com>
2024-11-05 02:02:42 +0300

    update main

--- api-gateway/cmd/main.go
+++ api-gateway/cmd/main.go
@@ -1,27 +1,32 @@
 package main
 
 import (
+	"flag"
 	"log"
-	"net/http"
+	"os"
 
-	"github.com/ncfex/dcart/api-gateway/internal/routes"
+	"github.com/ncfex/dcart/api-gateway/internal/infrastructure/config"
+	"github.com/ncfex/dcart/api-gateway/internal/server"
 )
 
-// TODO - use config to set server port, service urls
 func main() {
-	router, err := routes.NewRouter("http://0.0.0.0:8081")
+	configPath := flag.String("config", "config/config.example.yaml", "path to config file")
+	flag.Parse()
+
+	logger := log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+
+	cfg, err := config.LoadConfig(*configPath)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Failed to load configuration:", err)
 	}
 
-	handler := router.SetupRoutes()
-
-	server := &http.Server{
-		Addr:    ":8080",
-		Handler: handler,
+	srv, err := server.NewServer(cfg, logger)
+	if err != nil {
+		log.Fatal("Failed to load server configuration:", err)
 	}
 
-	if err := server.ListenAndServe(); err != nil {
-		log.Fatal(err)
+	logger.Printf("starting server on port: %s", cfg.Server.Port)
+	if err := srv.Server.ListenAndServe(); err != nil {
+		log.Fatal("Server failed:", err)
 	}
 }
