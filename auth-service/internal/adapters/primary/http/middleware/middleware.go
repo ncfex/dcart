1051ae30be938d941d1ec54238f1224221fdfa36
oguzhang <oguzhanguldibi@gmail.com>
2024-11-07 22:29:12 +0300

    update middleware

--- auth-service/internal/adapters/primary/http/middleware/middleware.go
+++ auth-service/internal/adapters/primary/http/middleware/middleware.go
@@ -1,36 +1,14 @@
 package middleware
 
-import (
-	"net/http"
-)
+import "net/http"
 
 type Middleware func(http.Handler) http.Handler
 
-type chain struct {
-	middlewares []Middleware
-}
-
-func NewChain(middlewares ...Middleware) *chain {
-	return &chain{
-		middlewares: append([]Middleware{}, middlewares...),
+func Chain(middlewares ...Middleware) Middleware {
+	return func(final http.Handler) http.Handler {
+		for i := len(middlewares) - 1; i >= 0; i-- {
+			final = middlewares[i](final)
+		}
+		return final
 	}
-}
-
-func (c *chain) Then(h http.Handler) http.Handler {
-	if h == nil {
-		h = http.DefaultServeMux
-	}
-
-	for i := len(c.middlewares) - 1; i >= 0; i-- {
-		h = c.middlewares[i](h)
-	}
-
-	return h
-}
-
-func (c *chain) ThenFunc(fn http.HandlerFunc) http.Handler {
-	if fn == nil {
-		return c.Then(nil)
-	}
-	return c.Then(fn)
 }
